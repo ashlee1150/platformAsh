ef1c36cbdfde2930a90d4785b6fc60429ed4baf0
ashlee1150 <44530892+ashlee1150@users.noreply.github.com>
2020-02-22 21:34:56 -0500

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,19 +1,13 @@
-//set the canvas "resolution" to match the screen
+// set the canvas "resolution" to match the screen
 let CANVAS = document.getElementById("display")
-console.log(CANVAS)
 CANVAS.width = document.body.clientWidth
 CANVAS.height = document.body.clientHeight
-//create 2d context object for drawing on the canvas
-let CTX = CANVAS.getContext("2d") //2d game
-function ERASE(){
-	//draw the rectangle 
-	//console so we can see in inspect thingy 
-	console.log(CTX)
-	CTX.fillStyle = "pink"
-	CTX.fillRect(0, 0, CANVAS.width, CANVAS.height)
 
+// create 2D context object for drawing on the CANVAS
+let CTX = CANVAS.getContext("2d")
 
-
-	CTX.fillStyle = "purple"
-	CTX.fillRect(0,0,400, 100)
+// draw a blue rectangle the size of the screen
+function ERASE() {
+  CTX.fillStyle = "pink"
+  CTX.fillRect(0, 0, CANVAS.width, CANVAS.height)
 }
--- game.js
+++ game.js
@@ -1,36 +1,38 @@
-let hero = new Hero()
-let shapes = []
-ERASE()
-hero.draw()
-window.addEventListener("keydown",event=>{
-	if (event.code === "KeyS"){
-		//console.log(event.)
-		let colorIndex = Math.random()*100
-		
-		let color = "red"
-		if (colorIndex < 33){
-			color = "blue"
-		}if (colorIndex > 33 && colorIndex < 66){
-			color = "green"
-		}if (colorIndex>66){
-			color = "purple"
-		}
-		CTX.fillStyle = color
-		let x = Math.random() * CANVAS.width //changes to print to the edge of the screen 
-		let y = Math.random() * CANVAS.height //changes to print to edge of the screen cuz of canvas shit
-		let width = Math.random()* CANVAS.width
-		let height = Math.random()* CANVAS.height
-		let s = new Shape(x,y,width,height)
-		shapes.push(s)
 
-	}
-	if (event.code === "ArrowRight"){
-		hero.moveRight()
-	}
-	ERASE()
-	//console.log(shapes)
-	shapes.forEach(s => s.draw())
-	hero.draw()
+// state
+let hero = new Hero()
+let keyPressed = {}
+
+// setup a function to run whenever a key is pressed
+window.addEventListener("keydown", event => {
+  keyPressed[event.code] = true
+})
+window.addEventListener("keyup", event => {
+  keyPressed[event.code] = false
 })
 
+function loop() {
+  console.log(keyPressed)
+  if (keyPressed["ArrowUp"]) {
+    hero.moveUp()
+  }
+  if (keyPressed["ArrowDown"]) {
+    hero.moveDown()
+  }
+  if (keyPressed["ArrowLeft"]) {
+    hero.moveLeft()
+  }
+  if (keyPressed["ArrowRight"]) {
+    hero.moveRight()
+  }
 
+  // draw everything
+  ERASE()
+  hero.draw()
+
+  // run loop again!
+  setTimeout(loop, 1000 / 60)
+}
+
+// run loop once on startup to get it started
+loop()
